0814b0c808aafbebb90273ba179f669f7d7f7404
Pete Looney <petelooney81@gmail.com>
2020-11-17 23:07:27 -0600

    pair gif

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import pairLogoGif from '../img/pairLogo.gif';
 
 export default function Home() {
     return (
         <div>
-            <h1>Home Page</h1>
+            <div className="d-flex justify-content-center">
+                <img src={pairLogoGif} alt="Pair Logo gif"/>
+            </div>
+            <div className="d-flex flex-column align-items-center">
+                <h1>Welcome to Pair</h1>
+                <p>The Chat App for Pair Programmers</p>
+            </div>
         </div>
     )
 }
