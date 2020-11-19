c2c877eb055df18f2f101c836d85d507a873731e
Pete Looney <petelooney81@gmail.com>
2020-11-18 19:44:53 -0600

    react-markdown and input type to text area

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 import { db } from '../services/firebase';
 import './Messages.css';
 import {pairLogoURL} from '../services/usersDbFunctions';
+import ReactMarkdown from 'react-markdown';
 
 export default function Messages({message}) {
     const {user} = useSelector(state => state.user)
@@ -27,9 +28,10 @@ export default function Messages({message}) {
     }, [])
     
     //TODO: Time function to display the message time
-    
+    //TODO: React Syntax Highlighter on this page
+
     return (
-        <div className="mt-3">
+        <div className="m-3">
         {isCurrentUser ? (
             <div className="d-flex">
                 <div>
@@ -41,7 +43,7 @@ export default function Messages({message}) {
                         <span className="pb-1 ml-3">{`${time.getMonth() + 1}/${time.getDate()}/${time.getFullYear()} ${time.getHours()}:${time.getMinutes()}`}</span>
                     </div>
                     <div>
-                        {message.content}
+                        <ReactMarkdown source={message.content} />
                     </div>
                 </div>
             </div>
