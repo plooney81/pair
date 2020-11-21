fc52bdbf993485b8b4d134275be0687940daec2b
Pete Looney <petelooney81@gmail.com>
2020-11-20 22:35:47 -0600

    responsive front pic

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,30 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import pairLogoGif from '../img/pairLogo.gif';
+import './Home.css';
 
 export default function Home() {
-    //? Responsiveness addition
-    const [windowWidth, setWindowWidth] = useState(1200)
-    const mediaQuery = {
-        desktop: 1200,
-        tablet: 768,
-        phone: 576
-    }
-
-    useEffect(() => {
-        window.addEventListener('resize', () => {
-            setWindowWidth(document.body.clientWidth)
-        })
-    }, [])
-
-    const responsivePic = {
-        width: windowWidth < mediaQuery.phone ? '100%' : 'auto',
-        borderRadius: '.25rem'
-    }
-    
     return (
         <div>
             <div className="d-flex justify-content-center">
-                <img src={pairLogoGif} alt="Pair Logo gif" style={responsivePic} className="mt-5 logo-gif"/>
+                <img src={pairLogoGif} alt="Pair Logo gif" className="mt-5 logo-gif"/>
             </div>
             <div className="d-flex flex-column align-items-center">
                 <h1>Welcome to Pair</h1>
